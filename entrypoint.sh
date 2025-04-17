845351518661ca90138dd538b8baa92593b86089
Simone Tollardo <tollsimy.dev@protonmail.com>
2025-04-17 12:13:29 +0200

    Add localhost to OpenAPI servers in debug API spec

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 echo "Getting API specs..."
 rm -rf /tmp/OPP-common
 git clone -b main --depth 1 https://github.com/OpenParkProject/OPP-common.git /tmp/OPP-common
+echo "Adding localhost to servers"
+sed -i 's/servers:/servers:\n  - url: http:\/\/localhost:8080\/api\/v1\n    description: Local server/' /tmp/OPP-common/openapi.yaml
 
 echo "Generating API..."
 cd src
